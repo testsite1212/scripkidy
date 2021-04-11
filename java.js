1789880648adf60a24a0e573787f3747ab33dd1d
testsite1212 <82283132+testsite1212@users.noreply.github.com>
2021-04-11 11:46:58 +0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,11 @@
+document.querySelector('button').onclick = myClick;
+
+function myClick() {
+	alert('You are register:)');
+}
+
+function out() {
+	var h1;
+	h1 = document.getElementById('out');
+	h1.innerHTML = 'hhe'
+}
